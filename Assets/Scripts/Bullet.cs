b29f09baf8de2e8d47dbd8fdc842a10a0cf82b1c
carbolizer <carbolizer3@gmail.com>
2026-03-16 19:59:32 -0500

    erased the comment on bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed= 0.09f;
+    public float speed = 0.09f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
 
-        // This is cArlos, the code is working fine, but I want to make the bullet move faster. Can you help me with that?   
+           
     }
 
     // Update is called once per frame
